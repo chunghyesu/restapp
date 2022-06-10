3c823ab2aa715bdb21933990a2346aa512e93cca
hyesu <bobae04272@gmail.com>
2022-06-10 12:12:33 +0900

    create project and init code

---
+++ src/main/java/devfun/bookstore/common/config/AppConfig.java
@@ -0,0 +1,10 @@
+package devfun.bookstore.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig{
+	//데이터베이스접속, 트랜잭션관리 추가
+	//공통컨피그
+	//web.xml에서 설정한 ContextLoaderLisener에 의해 해당클래스 처리
+}
---
+++ src/main/java/devfun/bookstore/rest/config/RestAppConfig.java
@@ -0,0 +1,14 @@
+package devfun.bookstore.rest.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+public class RestAppConfig {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
